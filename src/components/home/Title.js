96335ddf055b9ece08a8f1f049f1a8b1eff9999c
HugoRaak <rathle.hugo@gmail.com>
2023-10-30 23:35:37 +0900

    fix: react-typed outdated
    use typed.js instead

--- src/components/home/Title.js
+++ src/components/home/Title.js
@@ -1,5 +1,5 @@
-import ReactTyped from "react-typed";
 import * as React from "react";
+import {TypingAnimate} from "./TypingAnimate";
 
 export const Title = () => {
     return <div className="flex-grow flex flex-col justify-center items-center">
@@ -7,13 +7,9 @@ export const Title = () => {
             <h3 className="text-2xl">Hello, I'm</h3>
             <h1 className="text-6xl font-bold text-orange py-2">Hugo Rathle.</h1>
             <h1 className="text-6xl font-bold opacity-40 dark:opacity-70 py-2">A full-stack developer.</h1>
-            <div className="flex py-2">
-                <h2 className="text-4xl">I'm{" "}
-                <ReactTyped strings={["passionate", "versatile", "serious", "creative", "curious"]}
-                            typeSpeed={100} backSpeed={50} loop
-                            className="text-orange"/>
-                </h2>
-            </div>
+            <h2 className="text-4xl py-2">I'm{" "}
+                <TypingAnimate/>
+            </h2>
         </div>
     </div>
 }
---
+++ src/components/home/TypingAnimate.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Typed from "typed.js";
+
+export const TypingAnimate = () => {
+    const el = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ["passionate", "versatile", "serious", "creative", "curious"],
+            typeSpeed: 100,
+            backSpeed: 50,
+            loop: true
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
+    return <span ref={el} className="text-orange"/>
+}
